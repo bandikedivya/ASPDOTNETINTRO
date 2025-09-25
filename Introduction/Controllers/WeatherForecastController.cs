7c5d701f07d0b44f89b2df9b26326875fffef427
bandikedivya <divyacrkdv@gmail.com>
2025-09-25 12:53:15 +0530

    IActionResult code added

--- Introduction/Controllers/WeatherForecastController.cs
+++ Introduction/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
@@ -5,13 +6,29 @@ namespace Introduction.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EmployeesController
+    public class EmployeesController: ControllerBase
     {
         [HttpGet]
         [Route("GetEmpName")]
-        public string GetEmpName()
+        //public string GetEmpName()
+        //{
+        //    return "Isabel";
+        //}
+        //public List<int> GetEmpName()
+        //{
+        //    return new List<int> { 1,2,3,4,5};
+        //}
+
+        //IActionResult is used to get the status of the Api like 404: Not Found, 200: Ok
+        // The NotFound and OK are the methods of the IActionResult. To use this should inherit the ControllerBase(class)
+        public IActionResult GetEmpName()
         {
-            return "Isabel";
+            string empname = "Divya";
+            if (empname == null)
+            {
+                return NotFound(new { Message = "Error" });
+            }
+            return Ok(new { Message = "Employee is Found" });
         }
 
         [HttpGet]
@@ -21,6 +38,6 @@ namespace Introduction.Controllers
             return "Conrad";
         }
 
-        //https://localhost:7287/Employees/GetEmpName
+        //https://localhost:7287/api/Employees/GetEmpName
     }
 }
--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -3,6 +3,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//builder.Services.AddCors((cors) =>
+//{
+//    cors.AddPolicy("AllowLocalhost4200", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:4200/").AllowAnyHeader().AllowAnyMethod();
+//    });
+//});
+builder.Services.AddCors((cors) =>
+{
+    cors.AddPolicy("AllowLocalhost4200", policy =>
+    {
+        policy.
+        WithOrigins("http://localhost:4200").
+        AllowAnyHeader().
+        AllowAnyMethod();
+    });
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +36,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("AllowLocalhost4200");
 
 app.UseHttpsRedirection();
 
