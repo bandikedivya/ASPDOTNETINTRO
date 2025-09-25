67c726afedca8879ba7ecbe33805185c45fd6112
bandikedivya <divyacrkdv@gmail.com>
2025-09-25 18:40:49 +0530

    Singleton, Scoped and Transient Code added

---
+++ Introduction/Services/CoffeeServices.cs
@@ -0,0 +1,18 @@
+﻿namespace Introduction.Interfaces;
+
+public class CoffeeServices : ISingletonCoffee, IScopedCoffee, ITransientCoffee
+{
+    private readonly string _coffeeId;
+
+
+    public CoffeeServices()
+    {
+        _coffeeId = Guid.NewGuid().ToString();
+    }
+
+
+    public string GetCoffeeId()
+    {
+        return _coffeeId;
+    }
+}
