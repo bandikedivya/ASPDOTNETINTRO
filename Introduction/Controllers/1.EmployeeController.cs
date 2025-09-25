656687d0a70cc8af9c2820fad2dedae9060c4a97
bandikedivya <divyacrkdv@gmail.com>
2025-09-25 18:17:50 +0530

    HTTP Verbs code added

---
+++ Introduction/Controllers/1.EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.Controllers.Employee_1
+{
+
+    [ApiController]   //This is kind of attribute where asp.net core server thinks that class is able to create the endpoints/resources.
+
+    // ControllerBase is a class which is useful to prepare the status to the responses.
+    public class EmployeeV1Controller : ControllerBase   
+    {
+        // to prepare the endpoints, we should make sure that, to design with the asynchronous calls 
+    }
+}
